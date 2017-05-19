4ca731c81372ee981405fdfa0e1c320da3f2393d
admin <admin@PC105>
2017-05-19 14:20:51 +0900

    ヘリの種類を増やした

--- src/simpleshooting/EnemyHeri1.java
+++ src/simpleshooting/EnemyHeri1.java
@@ -2,20 +2,17 @@ package simpleshooting;
 
 import java.awt.Graphics;
 
-public class EnemyHeri extends Enemy {
+public class EnemyHeri1 extends Enemy {
 
-	private int cntFrame;
-
-	public EnemyHeri(int x, int y) {
+	public EnemyHeri1(int x, int y) {
 		super(x, y);
 		this.life = 1;
 		this.x = x;
 		this.y = y;
 		this.width = 46;
 		this.height = 50;
-		this.setType(ObjectType.enemy);
+		this.setType(ObjectType.ENEMY);
 		this.img = MyInterface.Imageset.ENEMY_HERI;
-		this.cntFrame = 0;
 	}
 
 	@Override
@@ -23,7 +20,6 @@ public class EnemyHeri extends Enemy {
 		if (y > MyInterface.GAME_HEIGHT) {
 			delete();
 		}
-		cntFrame++;
 		y += moveSpeed;
 
 	}
@@ -33,7 +29,7 @@ public class EnemyHeri extends Enemy {
 		if (!isAlive() || img == null)
 			return;
 		g.drawImage(img, x - width / 2, y - height / 2, x + width / 2, y + height / 2, 63, 0, 109, 53, null);
-		if (this.cntFrame % 6 < 3) {
+		if (Field.getFrame() % 6 < 3) {
 			g.drawImage(img, x - 24, y - 15, x + 24, y + 15, 117, 0, 166, 30, null);
 		} else {
 			g.drawImage(img, x - 15, y - 24, x + 15, y + 24, 128, 46, 157, 93, null);
---
+++ src/simpleshooting/EnemyHeri2.java
@@ -0,0 +1,33 @@
+package simpleshooting;
+
+import java.awt.Graphics;
+import java.awt.Image;
+
+public class EnemyHeri2 extends EnemyHeri1 {
+
+	private Image img;
+
+	public EnemyHeri2(int x, int y) {
+		super(x, y);
+		this.life = 1;
+		this.x = x;
+		this.y = y;
+		this.width = 48;
+		this.height = 48;
+		this.setType(ObjectType.ENEMY);
+		this.img = MyInterface.Imageset.ENEMY_HERI;
+	}
+
+	@Override
+	protected void draw(Graphics g, int cntFrame) {
+		if (!isAlive() || img == null)
+			return;
+		g.drawImage(img, x - width / 2, y - height / 2, x + width / 2, y + height / 2, 0, 0, 48, 48, null);
+		if (Field.getFrame() % 6 < 3) {
+			g.drawImage(img, x - 24, y - 15, x + 24, y + 15, 117, 0, 166, 30, null);
+		} else {
+			g.drawImage(img, x - 15, y - 24, x + 15, y + 24, 128, 46, 157, 93, null);
+		}
+	}
+
+}
